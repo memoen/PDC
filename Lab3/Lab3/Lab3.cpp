79fc7d85e0c2139e5248566ce95b10d7aeff2049
Oleksandr Hryshchuk <Oleksandr.Hryshchuk@mask-me.net>
2020-11-29 17:32:19 +0200

    Lab 4 MPI

---
+++ Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,197 @@
+﻿// Lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+#include <iostream>
+#include <omp.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+    
+    uint64_t g, e, p;
+    cin >> g;
+    cin >> e;
+    cin >> p;
+
+
+    /*uint64_t _64bitVariable;
+    cin >> _64bitVariable;*/
+/*    uint64_t v = e;
+    uint8_t i = 0, parts[8] = { 0 };
+    do parts[i++] = v & 0xFF; while (v >>= 8);
+
+
+    uint8_t results[8] = { 0 };
+#pragma omp parallel num_threads(8)
+#pragma omp for
+    for (int j = 0; j < 8; j++) 
+    {
+        results[j] = (int8_t)pow(g, pow(2,j))%p;
+        results[j] = (int8_t)pow(results[j],parts[j]) % p;
+    }
+
+
+
+    cout << endl;
+}
+*/
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+#include <omp.h>
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+uint16_t inline mpow2(uint16_t shftAmt) {
+    return (1 << shftAmt);
+}
+
+int main()
+{
+    cout << omp_get_num_threads() << endl << omp_get_max_threads();
+    omp_set_num_threads(4);
+    cout << omp_get_num_threads() << endl << omp_get_max_threads();
+    uint64_t g;
+    uint64_t e;
+    uint64_t p;
+
+    uint16_t resultArray[4] = { 0, 0, 0, 0 };
+    uint64_t result = 0;
+
+    cout << "Enter g: ";
+    cin >> g;
+    cout << "Enter e: ";
+    cin >> e;
+    cout << "Enter p: ";
+    cin >> p;
+
+    double startTime, endTime, ticks;
+
+    uint16_t shiftArray[4] = { 0, 0, 0, 0 };
+    shiftArray[0] = e;
+    for (int i = 1; i < sizeof(shiftArray) / sizeof(shiftArray[0]); i++)
+    {
+        shiftArray[i] = e >> 16 * i;
+    }
+    int index = -1;
+    for (int i = 0; i < sizeof(shiftArray) / sizeof(shiftArray[0]); i++)
+    {
+        if (shiftArray[i] == 0)
+        {
+            int j = i + 1;
+            for (; j < sizeof(shiftArray) / sizeof(shiftArray[0]); j++)
+            {
+                if (shiftArray[j] == 0)
+                    continue;
+                break;
+            }
+            if (j == sizeof(shiftArray) / sizeof(shiftArray[0]))
+            {
+                index = i;
+                break;
+            }
+        }
+    }
+
+    auto r0 = resultArray[0];
+    auto r1 = resultArray[1];
+    auto r2 = resultArray[2];
+    auto r3 = resultArray[3];
+    startTime = omp_get_wtime();
+
+
+/*#pragma omp parallel for num_threads(4)// private(p,g,index)
+    for (int i = 0; i < sizeof(shiftArray) / sizeof(shiftArray[0]); i++)
+    {
+        if (i >= index)
+        {
+            resultArray[i] = 0;
+        }
+        else
+        {
+            resultArray[i] = (int64_t)pow(g, pow(2, i)) % p;
+            resultArray[i] = (int64_t)pow(resultArray[i], shiftArray[i]) % p;
+        }
+    }*/
+
+
+
+#pragma omp parallel sections private(p,g)
+    {
+        #pragma omp section 
+        {
+            //printf("id = %d, \n", omp_get_thread_num());
+            if (0 >= index)
+            {
+                r0 = 0;
+            }
+            else
+            {
+                r0 = (int64_t)pow(g, mpow2(0)) % p;
+                r0 = (int64_t)pow(r0, r0) % p;
+            }
+        }
+        #pragma omp section
+        {
+            //printf("id = %d, \n", omp_get_thread_num());
+            if (1 >= index)
+            {
+                r1 = 0;
+            }
+            else
+            {
+                r1 = (int64_t)pow(g, mpow2(1)) % p;
+                r1 = (int64_t)pow(r1, r1) % p;
+            }
+        }
+        #pragma omp section 
+        {
+            //printf("id = %d, \n", omp_get_thread_num());
+            if (2 >= index)
+            {
+                r2 = 0;
+            }
+            else
+            {
+                r2 = (int64_t)pow(g, mpow2(2)) % p;
+                r2 = (int64_t)pow(r2, r2) % p;
+            }
+        }
+        #pragma omp section 
+        {
+            //printf("id = %d, \n", omp_get_thread_num());
+            if (3 >= index)
+            {
+                r3 = 0;
+            }
+            else
+            {
+                r3 = (int64_t)pow(g, mpow2(3)) % p;
+                r3 = (int64_t)pow(r3, r3) % p;
+            }
+        }
+    }
+
+    endTime = omp_get_wtime();
+    ticks = omp_get_wtick();
+
+    for (int i = sizeof(resultArray) / sizeof(resultArray[0]) - 1; i >= 0; i--)
+    {
+        result += (uint64_t)resultArray[i] << (16 * i);
+    }
+
+    result = result % p;
+    cout << "Result: " << result << "\n";
+    cout << "Duration: " << endTime - startTime << "\n";
+    cout << "Timer accuracy: " << ticks << "\n";
+}
---
+++ Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,145 @@
+﻿/*
+ * This file is part of the PDC_Labs distribution (https://github.com/xxxx or http://xxx.github.io).
+ * Copyright (c) 2020 Oleksandr Hryshchuk.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h>
+#include "mpi.h"
+#include <math.h>
+
+using namespace std;
+
+/// <summary>
+/// Sin function using Taylor series for n = 3
+/// </summary>
+/// <param name="x"></param>
+/// <returns>Sin(x)</returns>
+double custom_sin(double x) 
+{
+    double x_3 = x * x * x;
+    double x_5 = x_3 * x * x;
+
+    return x - x_3 / 6 + x_5 / 120;
+}
+
+/// <summary>
+/// Cos function using Taylor series for n = 3
+/// </summary>
+/// <param name="x"></param>
+/// <returns>Cos(x)</returns>
+double custom_cos(double x) 
+{
+    double x_2 = x * x;
+
+    return 1 - x_2 / 2 + x_2*x_2 / 24;
+}
+
+/// <summary>
+/// Generate a coeficient
+/// </summary>
+/// <param name="size">size of array</param>
+/// <returns>Generated array</returns>
+double * genrate_random_array(int size) 
+{
+    double * array = new double[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        array[i] = ((double(rand()) / double(RAND_MAX)) * 2) - 1;
+    }
+    return array;
+}
+
+int main(int *argc, char * argv[])
+{
+    int N = atoi(argv[1]);
+
+    int numtasks, pid, elements_per_process, n_elements_recieved;
+    MPI_Status status;
+
+    MPI_Init(argc, &argv);
+    double t1, t2;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+    elements_per_process = N / numtasks;    
+    // master process 
+    if (pid == 0) {
+        t1 = MPI_Wtime();
+        int index, i;
+        //cout << "Total number of process: \t" << numtasks << endl;
+        // check if more than 1 processes are run 
+        if (numtasks > 1) {
+            // distributes the portion of array 
+            // to child processes to calculate 
+            // their partial sums 
+            for (i = 1; i < numtasks - 1; i++) {
+                index = i * elements_per_process;
+
+                MPI_Send(&elements_per_process, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            }
+
+            // last process adds remaining elements 
+            index = i * elements_per_process;
+            int elements_left = N - index;
+
+            MPI_Send(&elements_left, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+        }
+
+        // master process add its own sub array 
+        double sum = 0;
+        for (i = 0; i < elements_per_process; i++)
+            sum += (((double(rand()) / double(RAND_MAX)) * 2) - 1) * cos(i * 0.0001);
+
+        // collects partial sums from other processes 
+        int tmp;
+        for (i = 1; i < numtasks; i++) {
+            MPI_Recv(&tmp, 1, MPI_INT,
+                MPI_ANY_SOURCE, 0,
+                MPI_COMM_WORLD,
+                &status);
+            int sender = status.MPI_SOURCE;
+
+            sum += tmp;
+        }
+        //printf("Sum of array is : %d\n", sum);
+        t2 = MPI_Wtime();
+        cout << "{" << N << ", " << t2-t1<<"}, ";
+    }
+    else {
+        MPI_Recv(&n_elements_recieved,
+            1, MPI_INT, 0, 0,
+            MPI_COMM_WORLD,
+            &status);
+
+        // calculates its partial sum 
+        int partial_sum = 0;
+        for (int i = 0; i < n_elements_recieved; i++)
+            partial_sum += (((double(rand()) / double(RAND_MAX)) * 2) - 1) *cos((i+pid*elements_per_process)*0.001);
+
+        // sends the partial sum to the root process 
+        MPI_Send(&partial_sum, 1, MPI_INT,
+            0, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();   
+}
+
+ 
+
